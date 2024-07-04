daea41f24df097c5a7e0366bd7f38933b4bad9b5
Alvi Iskanderov <iskanderov.a@it-webdevelopment.com>
2024-07-04 21:49:13 +0300

    fix db connection

--- connection.go
+++ connection.go
@@ -15,16 +15,24 @@ func ConnectDb() (*sql.DB, error) {
 	if err != nil {
 		log.Fatalf("ошибка при загрузке .env.local файла: %v\n", err)
 	}
-	connStr := os.Getenv("DATABASE_URL")
-	if connStr == "" {
-		return nil, fmt.Errorf("строка подключения к базе данных не задана")
-	}
 
-	// Подключитесь к базе данных
-	db, err := sql.Open("postgres", connStr)
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	fmt.Println("Параметры подключения: ", psqlInfo)
+
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, fmt.Errorf("не удалось подключиться к базе данных: %w", err)
 	}
+
+	fmt.Println("Объект db после sql.Open: ", db)
 
 	err = db.Ping()
 	if err != nil {
